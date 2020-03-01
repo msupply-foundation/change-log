de3511433fde8389bc30aec0ac07b3f65b8cf2dc
nmadruga <katinachovansky@gmail.com>
2020-03-02 11:43:47 +1300

    Change function syntax and name

--- changesLog.js
+++ changesLog.js
@@ -5,9 +5,7 @@ const { fetchIssuesInMilestone, generateChangesLog } = require('./helpers');
 
 const commaSeparatedList = value => value.split(',');
 
-function checkValidStates(value) {
-  return (value === 'closed') || (value === 'open') || (value === 'all') ? value : undefined;
-}
+const validateState = value => (value === 'closed') || (value === 'open') || (value === 'all') ? value : undefined;
 
 commander
   .version('1.0.0', '-v, --version')
@@ -15,7 +13,7 @@ commander
   .requiredOption('-m, --milestone <number>', 'must filter issue from milestone')
   .option('-c, --customer <customer>, will filter the issues related to a customer if this option is received')
   .option('-i, --includeAll <flag>', 'will include a section with issues done for all clients (Default: false)')
-  .option('-s, --state <state>', 'should use issues with state: closed, open, all (Default: closed)', checkValidStates)
+  .option('-s, --state <state>', 'should use issues with state: closed, open, all (Default: closed)', validateState)
   .option('-f, --filters <labels>', 'comma separated labels to filter and group by issues', commaSeparatedList)
   .option('-d, --duplicate <allow>', 'should allow duplicated issues in different groups (Default: true)')
   .parse(process.argv);
