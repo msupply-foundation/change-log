8a2a73bf077e2710750c8c87f3d829403771fa6a
nmadruga <katinachovansky@gmail.com>
2020-03-04 09:56:58 +1300

    Alphabetical order on the list of constant strings

--- constants/logs.js
+++ constants/logs.js
@@ -1,42 +1,42 @@
 const OUTPUT = {
-    TOKEN_INVALID: 'Invalid GitHub authentication token!',
+    CHANGE_LOG_INIT: '# ChangeLog for Milestone: ',
+    CHANGES_FOR_CUSTOMER: '\n### For customer: ',
+    CHANGES_FOR_ALL: '\n### For all customers:',
+    DOTS: '...',
+    DUPLICATED_ISSUES: 'Duplicating issues when has duplicated labels.',
+    EXCLAMATION: '!',
     FETCH_MILESTONES_ISSUES: '\nFetching issues in Milestone: ',
     FETCHED_MILESTONES_ISSUES: '\nFetched issues by filters in Milestone: ',
     FETCH_SUCCESS: '\nSuccessful fetch!',
     FETCH_FAILED: '\nFailled to fetch!',
-    DUPLICATED_ISSUES: 'Duplicating issues when has duplicated labels.',
-    SINGLE_ISSUES: 'Not duplicating issues.',
-    STARTED_GROUPED_CHANGES_LOG: 'Creating Changes-log for issues based on groups: ',
-    STARTED_SINGLE_CHANGES_LOG: 'Creating Changes-log for issues without groups.',
-    SAVED_FILE_CHANGES_LOG: '\nThe file has been saved!',
-    CHANGE_LOG_INIT: '# ChangeLog for Milestone: ',
-    CHANGES_FOR_CUSTOMER: '\n### For customer: ',
-    CHANGES_FOR_ALL: '\n### For all customers:',
     ISSUES_TYPE: '\nIssues type: ',
     ISSUES_COUNT: 'Issues count: ',
     ISSUES_FOR_CUSTOMER: ' issues for customer: ',
     ISSUES_FOR_ALL_CUSTOMERS: ' issues for all customers.',
-    DOTS: '...',
-    NEWLINE: '\n',
-    EXCLAMATION: '!',
-    LIST: '- ',
     ISSUE_NUMBER_START: ' [',
     ISSUE_NUMBER_END: ']',
-    SEPARATOR: '\n--------'
+    LIST: '- ',
+    NEWLINE: '\n',
+    SINGLE_ISSUES: 'Not duplicating issues.',
+    SAVED_FILE_CHANGES_LOG: '\nThe file has been saved!',
+    SEPARATOR: '\n--------',
+    STARTED_GROUPED_CHANGES_LOG: 'Creating Changes-log for issues based on groups: ',
+    STARTED_SINGLE_CHANGES_LOG: 'Creating Changes-log for issues without groups.',
+    TOKEN_INVALID: 'Invalid GitHub authentication token!',
 }
 
 const proccessLogs = params => {
     return {
-        token_invalid: OUTPUT.TOKEN_INVALID,
+        duplicate_issues: OUTPUT.DUPLICATED_ISSUES + OUTPUT.NEWLINE,
         fetch_starts: OUTPUT.FETCH_MILESTONES_ISSUES + params.milestone + OUTPUT.DOTS + OUTPUT.NEWLINE,
         fetch_finished: OUTPUT.FETCHED_MILESTONES_ISSUES + params.milestone + OUTPUT.EXCLAMATION + OUTPUT.NEWLINE,
         fetch_success: OUTPUT.FETCH_SUCCESS,
         fetch_failed: OUTPUT.FETCH_FAILED,
-        duplicate_issues: OUTPUT.DUPLICATED_ISSUES + OUTPUT.NEWLINE,
+        saved_file_changes_log: OUTPUT.SAVED_FILE_CHANGES_LOG,
         single_issues: OUTPUT.SINGLE_ISSUES + OUTPUT.NEWLINE,
         starts_grouped_changes_log: OUTPUT.STARTED_GROUPED_CHANGES_LOG + params.filter + OUTPUT.DOTS + OUTPUT.NEWLINE,
         starts_single_changes_log: OUTPUT.STARTED_SINGLE_CHANGES_LOG + OUTPUT.DOTS + OUTPUT.NEWLINE,
-        saved_file_changes_log: OUTPUT.SAVED_FILE_CHANGES_LOG,
+        token_invalid: OUTPUT.TOKEN_INVALID,
     };
 };
 
@@ -63,6 +63,6 @@ const logIssuesCount = (group) => {
 module.exports = {
     initChangeLog,
     issuesChangeLog,
-    proccessLogs,
     logIssuesCount,
+    proccessLogs,
 };
