cb3f99f3baa50b003ab0737ec67b09e8870ad2d4
nmadruga <katinachovansky@gmail.com>
2020-03-04 09:42:12 +1300

    Update usage of labels as constans

--- constants/index.js
+++ constants/index.js
@@ -1,10 +1,10 @@
 const { TEXT_PATH } = require('./path');
-const { PREFIXES } = require('./labels');
+const { LABEL_GROUPS } = require('./labels');
 const { getTitle, checkSomeLabelEquals, checkSomeLabelHasPrefix } = require('./labels');
 const { initChangeLog, issuesChangeLog, proccessLogs, logIssuesCount } = require('./logs');
 
 module.exports = {
-  PREFIXES,
+  LABEL_GROUPS,
   TEXT_PATH,
   getTitle,
   checkSomeLabelEquals,
